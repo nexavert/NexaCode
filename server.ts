e5ab91865ff68845373a455908db4e21b627bca6
wvaz-nexavert <wilian.vaz@nexavert.com.br>
2025-08-09 20:47:02 -0300

    feat(build): Add root server.ts for Cloudflare Pages integration

---
+++ server.ts
@@ -0,0 +1,16 @@
+// server.ts
+import { logDevReady } from '@remix-run/cloudflare';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
+
+if (process.env.NODE_ENV === 'development') {
+  logDevReady(build);
+}
+
+export const onRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext: (context) => ({
+    ...context.env,
+  }),
+  mode: process.env.NODE_ENV,
+});
